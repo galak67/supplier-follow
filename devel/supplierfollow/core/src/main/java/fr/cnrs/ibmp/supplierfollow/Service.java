9f2b5faf3b735f576fc78472862d4c57ba28ad85
Gael Pawlak <gael.pawlak@ibmp-cnrs.unistra.fr>
2013-02-21 18:00:40 +0100

    add comment on service

--- devel/supplierfollow/core/src/main/java/fr/cnrs/ibmp/supplierfollow/Service.java
+++ devel/supplierfollow/core/src/main/java/fr/cnrs/ibmp/supplierfollow/Service.java
@@ -1,7 +1,16 @@
 package fr.cnrs.ibmp.supplierfollow;
 
+/**
+ * Example service
+ * 
+ * @author gael.pawlak@ibmp-cnrs.unistra.fr
+ */
 public interface Service {
 	
+	/**
+	 * Return a message
+	 * @return a message
+	 */
 	String getMessage();
 
 }
